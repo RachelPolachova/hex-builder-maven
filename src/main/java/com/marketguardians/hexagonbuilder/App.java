6e32b1fa25618ed63eeec60c36acd9b9da0afbb1
Rachel Polachova <polachova@d-tt.nl>
2020-03-03 21:47:05 +0100

    kralove hradecky done

--- src/main/java/com/marketguardians/hexagonbuilder/App.java
+++ src/main/java/com/marketguardians/hexagonbuilder/App.java
@@ -15,10 +15,10 @@ public class App  {
 
     public static void handleOwnJSON() {
         JSONReader jsonReader = new JSONReader();
-        MatrixConfiguration matrixConfiguration = jsonReader.readOwnJson("praha.json");
+        MatrixConfiguration matrixConfiguration = jsonReader.readOwnJson("kralovehradecky.json");
         HexagonBuilder hexagonBuilder = new HexagonBuilder();
         hexagonBuilder.handleMatrixConf(matrixConfiguration);
-        jsonReader.write("praha-hexagons.json", hexagonBuilder.getHandledHexagons());
+        jsonReader.write("kralovehradecky-hexagons.json", hexagonBuilder.getHandledHexagons());
     }
 
     public static void spracujKraje() {
